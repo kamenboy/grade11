74bd7170b53535ce7b5d754a43412fe14a4ec437
kamenboy <kamen.nikolov24@gmail.com>
2024-12-28 19:33:47 +0200

    Create imsFrame.java

---
+++ Inventory Managment System/imsFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class imsFrame extends JFrame {
+    imsFrame(){
+        JFrame frame = new JFrame();
+        this.setTitle("Inventory Management System");
+        this.setSize(1000, 700);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+}
