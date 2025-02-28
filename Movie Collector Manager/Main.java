824dbb0598d1dd76502d0742404a2778fb2c5953
kamenboy <kamen.nikolov24@gmail.com>
2025-02-28 08:58:30 +0200

    Update Main.java

--- Movie Collector Manager/Main.java
+++ Movie Collector Manager/Main.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-public class Main{
-    public static void main(String[] args){
+        new GUI();
 
+        Movie[] movies = {
+                new Movie("Fast and Furious", "Action"),
+                new Movie("Harry Potter", "Fantasy"),
+                new Movie("The Wolf of Wall Street", "Comedy/Thriller")
+        };
+
+    }
+}
+
+class Movie {
+    private String title;
+    private String genre;
+
+    public Movie(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 }
