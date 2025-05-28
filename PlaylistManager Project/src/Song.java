d51b55bd7db292610eaff9e4bbe46738102295b0
kamenboy <kamen.nikolov24@gmail.com>
2025-05-28 09:14:27 +0300

    Update Song.java

--- PlaylistManager Project/src/Song.java
+++ PlaylistManager Project/src/Song.java
@@ -1,1 +1,28 @@
+public class Song {
+    private String title;
+    private String artist;
+    private int duration;
+    private String genre;
+
+    public Song(String title, String artist, int duration, String genre) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.genre = genre;
+    }
+
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public int getDuration() { return duration; }
+    public String getGenre() { return genre; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setArtist(String artist) { this.artist = artist; }
+    public void setDuration(int duration) { this.duration = duration; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String toString() {
+        return title + " - " + artist + " (" + duration + "s)";
+    }
+}
 
