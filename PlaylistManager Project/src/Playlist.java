755ba9a4232c245ef839e97c4730693142a835de
kamenboy <kamen.nikolov24@gmail.com>
2025-05-28 09:14:43 +0300

    Update Playlist.java

--- PlaylistManager Project/src/Playlist.java
+++ PlaylistManager Project/src/Playlist.java
@@ -1,1 +1,20 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<Song> songs;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getName() { return name; }
+    public ArrayList<Song> getSongs() { return songs; }
+
+    public void addSong(Song song) { songs.add(song); }
+    public void removeSong(int index) {
+        if (index >= 0 && index < songs.size()) songs.remove(index);
+    }
+}
 
